e25c7f3cc0bfe2defbd63cc6c309e51c5a44ad97
Deltazoe <138903048+Deltazoe@users.noreply.github.com>
2023-11-14 13:49:01 -0500

    Finished main menu

--- Vivis/UI/MainMenu/main_menu.gd
+++ Vivis/UI/MainMenu/main_menu.gd
@@ -10,10 +10,13 @@ func _on_StartButton_pressed():
 
 
 func _on_settings_pressed():
-#	var settings = load("res://UI/MainMenu/settings_menu.tscn").instance()
-#	get_tree().current_scene.add_child(settings)
-	pass
+	get_tree().change_scene_to_file("res://UI/MainMenu/settings_menu.tscn")
+	
 
 
 func _on_exit_pressed():
 	get_tree().quit()
+
+
+func _on_credits_pressed():
+	get_tree().change_scene_to_file("res://UI/MainMenu/Credits.tscn")
