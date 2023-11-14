0313afbacef016c7666011e6cbf8523c5385b461
Deltazoe <138903048+Deltazoe@users.noreply.github.com>
2023-11-14 14:34:54 -0500

    added Win Screen

---
+++ Vivis/UI/WinScreen/WiinScreen.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_exit_pressed():
+	get_tree().quit()
+
+
+func _on_restart_pressed():
+	get_tree().change_scene_to_file("res://UI/MainMenu/main_menu.tscn")
