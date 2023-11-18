d41a5caf3b0ef97386b6e1018b54a693b10836fc
Zonnap <Zonnap@outlook.com>
2023-11-18 02:32:56 -0500

    Added Cover Art & Corrected Scaling
    Added Cover art and corrected scaling so that the images arent whack

--- Vivis/UI/MainMenu/main_menu.gd
+++ Vivis/UI/MainMenu/main_menu.gd
@@ -4,6 +4,9 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	
+func _process(delta):
+		$Node2D/AnimationPlayer.play("Text_Animation")
 
 func _on_StartButton_pressed():
 	get_tree().change_scene_to_file("res://levels/PlayerTestingLevel.tscn")
