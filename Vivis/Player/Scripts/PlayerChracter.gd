3bb7f5b0d73444530798f03d79e7091a55405604
Deltazoe <138903048+Deltazoe@users.noreply.github.com>
2023-11-18 13:50:52 -0500

    pause menu added

--- Vivis/Player/Scripts/PlayerChracter.gd
+++ Vivis/Player/Scripts/PlayerChracter.gd
@@ -86,3 +86,10 @@ func _on_thorn_body_entered(body):
 
 func _on_fish_snacks_body_entered(body):
 	pass
+	#-----------------------------------------------------X
+#-------------------------------------------------------- Pause
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		$PauseMenu.pause()
+		
